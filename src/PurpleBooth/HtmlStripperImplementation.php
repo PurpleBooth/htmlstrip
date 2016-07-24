e983138a89f6560b855079178a23ce9227a52e16
Billie Thompson <billie@purplebooth.co.uk>
2016-07-24 19:54:17 +0100

    Switch to using an interface
    This will allow our users not to have to depend on a concrete
    implementation

---
+++ src/PurpleBooth/HtmlStripperImplementation.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace PurpleBooth;
+
+/**
+ * Parse HTML to plain text
+ *
+ * @package PurpleBooth
+ */
+class HtmlStripperImplementation implements HtmlStripper
+{
+    /**
+     * Parse the text.
+     *
+     * The actual logic of this function is to setup the XML parsing extension.
+     *
+     * @param string $html
+     *
+     * @return string
+     */
+    public function toText($html)
+    {
+        $parser = new Parser();
+
+        $xmlParser = xml_parser_create();
+        xml_set_element_handler($xmlParser, [$parser, 'startElement'], [$parser, 'endElement']);
+        xml_set_character_data_handler($xmlParser, [$parser, 'characterData']);
+
+        $wrappedHtml  = "<root>$html</root>";
+        $returnStatus = xml_parse($xmlParser, $wrappedHtml, true);
+
+        if (!$returnStatus) {
+            return $html;
+        }
+
+        return $parser->getText();
+    }
+}
